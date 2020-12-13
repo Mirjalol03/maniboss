968fcf6d08a53c0676e773848ad9a9297671d7af
mirjalol03 <jabborovmirjalol03@gmail.com>
2020-12-13 21:17:10 +0500

    salom

---
+++ script.js
@@ -0,0 +1,27 @@
+maniboss = document.getElementById("maniBosA");
+function maniBos () {
+    if (maniboss = maniboss) {
+        maniboss.style.color = "#000";
+        maniboss.style.transition = "0.3s";
+        maniboss.style.border = "#fff solid 5px";
+        document.querySelector('body').style.background = "rgb(123, 255, 0)";
+        document.getElementById("maniBosA").innerHTML = "Mani Ikki marta bos";
+    }
+}
+function manibos2 () {
+    document.getElementById("maniBosA").innerHTML = "Manini bosganing uchun Rahmat 😊";
+    document.querySelector('body').style.background = "dodgerblue";
+    let orqagadiv = document.createElement('h1');
+    let orqagaA = document.createElement('a');
+    orqagaA.innerHTML = "salom";
+    maniboss.style.border = "rgb(123, 255, 0) solid 5px";
+    orqagadiv.appendChild(orqagaA);
+    document.querySelector("body").appendChild(h1);
+}
+// function manibos2 () {
+//     maniboss.style.background = "rgb(123, 255, 0)";
+//     maniboss.style.color = "#fff";
+// }
+// function manibos3() {
+//     maniboss.style
+// }
